5b971b94f83d212b7b484e1b5af332ffdbef45f0
albasimia <albasimia888@gmail.com>
2026-04-01 00:00:38 +0900

    Emacsバインディングの実装を修正。テスト時はレイヤー変更を無効化。

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -33,6 +33,7 @@ static bool send_emacs_combo(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
+        // Special Emacs keys: send direct action without any modifier held
         case KC_A:
             tap_code(KC_HOME);
             return false;
@@ -59,7 +60,11 @@ static bool send_emacs_combo(uint16_t keycode, keyrecord_t *record) {
             tap_code(KC_DOWN);
             return false;
         default:
-            return true;
+            // Non-special keys: apply Ctrl (Win/Linux) or GUI (Mac) and send
+            register_code(aorazer_os_is_mac ? KC_LGUI : KC_LCTL);
+            tap_code(keycode);
+            unregister_code(aorazer_os_is_mac ? KC_LGUI : KC_LCTL);
+            return false;
     }
 }
 
@@ -72,9 +77,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case EMACS:
             if (record->event.pressed) {
                 emacs_held = true;
-                register_code(aorazer_os_is_mac ? KC_LGUI : KC_LCTL);
             } else {
-                unregister_code(aorazer_os_is_mac ? KC_LGUI : KC_LCTL);
                 emacs_held = false;
             }
             return false;
@@ -83,12 +86,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+// テスト時は一時的にLayer変更をKC_NOにする。
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_NO,              KC_NO,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,
-        KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    TG(_L2),            TG(_L5), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-        KC_PGDN, EMACS,   KC_A,    KC_S,    KC_D,    KC_F,      KC_G,    TG(_L3),            TG(_L6), KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
-        KC_HOME, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    TG(_L4),            TG(_L7), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
+        KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    KC_NO,            KC_NO, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+        KC_HOME, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    KC_NO,            KC_NO, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
+        KC_PGDN, EMACS,   KC_A,    KC_S,    KC_D,    KC_F,      KC_G,    KC_NO,            KC_NO, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
         KC_END,  MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, TG(_BASE), KC_SPC, KC_ESC,     KC_ENT,  KC_SPC, KC_BSPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
